fd085a8153d246df9cf4528ebda7379b6fea26d2
AdrianGerman <adrian.german1019@gmail.com>
2025-03-11 20:37:47 -0600

    changes in Main structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,56 +1,46 @@
+const sectionsData = [
+  {
+    name: "Amazon",
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+  },
+  {
+    name: "Mercado Libre",
+    img: "https://rappicard.mx/wp-content/uploads/2024/10/logo-mercado-libre.png"
+  },
+  {
+    name: "Aliexpress",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Aliexpress_logo.svg/2560px-Aliexpress_logo.svg.png"
+  },
+  {
+    name: "Temu",
+    img: "https://1000marcas.net/wp-content/uploads/2023/11/Temu-Logo.png"
+  },
+  {
+    name: "Steam",
+    img: "https://1000marcas.net/wp-content/uploads/2020/10/Steam-Logo.png"
+  },
+  {
+    name: "Otros",
+    img: "https://cdn-icons-png.flaticon.com/512/7580/7580377.png"
+  }
+]
+
 export const Main = () => {
   return (
-    <>
-      <section className="grid grid-cols-3 gap-4">
-        <div className="w-80 h-80 border-3 rounded-2xl flex flex-col items-center justify-center gap-4">
-          <p className="text-2xl">Amazon</p>
+    <section className="grid grid-cols-3 grid-rows-2 gap-4 h-screen p-4">
+      {sectionsData.map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center justify-center rounded-2xl shadow-2xl w-full h-full aspect-square overflow-hidden"
+        >
+          <p className="text-3xl font-bold">{item.name}</p>
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
-            alt="Amazon logo"
-            className="w-40"
+            src={item.img}
+            alt={`${item.name} logo`}
+            className="w-40 max-w-full"
           />
         </div>
-        <div className="w-80 h-80 border-3 rounded-2xl flex flex-col items-center justify-center gap-4">
-          <p className="text-2xl">Mercado libre</p>
-          <img
-            src="https://rappicard.mx/wp-content/uploads/2024/10/logo-mercado-libre.png"
-            alt="Mercado libre logo"
-            className="w-40"
-          />
-        </div>
-        <div className="w-80 h-80 border-3 rounded-2xl flex flex-col items-center justify-center gap-4">
-          <p className="text-2xl">Aliexpress</p>
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Aliexpress_logo.svg/2560px-Aliexpress_logo.svg.png"
-            alt="Aliexpress logo"
-            className="w-40"
-          />
-        </div>
-        <div className="w-80 h-80 border-3 rounded-2xl flex flex-col items-center justify-center gap-4">
-          <p className="text-2xl">Temu</p>
-          <img
-            src="https://1000marcas.net/wp-content/uploads/2023/11/Temu-Logo.png"
-            alt="Temu logo"
-            className="w-40"
-          />
-        </div>
-        <div className="w-80 h-80 border-3 rounded-2xl flex flex-col items-center justify-center gap-4">
-          <p className="text-2xl">Steam</p>
-          <img
-            src="https://1000marcas.net/wp-content/uploads/2020/10/Steam-Logo.png"
-            alt="Steam logo"
-            className="w-40"
-          />
-        </div>
-        <div className="w-80 h-80 border-3 rounded-2xl flex flex-col items-center justify-center gap-4">
-          <p className="text-2xl">Otros</p>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/7580/7580377.png"
-            alt="Other logo"
-            className="w-40"
-          />
-        </div>
-      </section>
-    </>
+      ))}
+    </section>
   )
 }
