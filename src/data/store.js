65cd8094883b266a4437084413556ad5f40e54ba
AdrianGerman <adrian.german1019@gmail.com>
2025-03-12 07:51:08 -0600

    separe data

---
+++ src/data/store.js
@@ -0,0 +1,26 @@
+export const sectionsData = [
+  {
+    name: "Amazon",
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"
+  },
+  {
+    name: "Mercado Libre",
+    img: "https://rappicard.mx/wp-content/uploads/2024/10/logo-mercado-libre.png"
+  },
+  {
+    name: "Aliexpress",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Aliexpress_logo.svg/2560px-Aliexpress_logo.svg.png"
+  },
+  {
+    name: "Temu",
+    img: "https://1000marcas.net/wp-content/uploads/2023/11/Temu-Logo.png"
+  },
+  {
+    name: "Steam",
+    img: "https://1000marcas.net/wp-content/uploads/2020/10/Steam-Logo.png"
+  },
+  {
+    name: "Otros",
+    img: "https://cdn-icons-png.flaticon.com/512/7580/7580377.png"
+  }
+]
