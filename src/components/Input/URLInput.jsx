d91d3be037287084b7a1173e95a8244aff319c11
AdrianGerman <adrian.german1019@gmail.com>
2025-03-14 07:39:57 -0600

    styles for inputs

--- src/components/Input/URLInput.jsx
+++ src/components/Input/URLInput.jsx
@@ -12,22 +12,31 @@ export const URLInput = ({ onAddUrl }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Ingrese el título del URL..."
-        required
-      />
-      <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="Ingrese URL aquí..."
-        required
-      />
-      <button type="submit">Agregar URL</button>
+    <form onSubmit={handleSubmit} className="space-y-4">
+      <section className="flex gap-4">
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Ingrese el nombre del producto..."
+          required
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+        />
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Ingrese URL aquí..."
+          required
+          className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+        />
+      </section>
+      <button
+        type="submit"
+        className="w-2/6 p-3 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
+      >
+        Agregar URL
+      </button>
     </form>
   )
 }
